e112052018d17841f0a88c964bedf1cc812ffe7c
LAPTOP-KUSH1VGI\ryo-t <ryo.takahashi.likeit@gmail.com>
2018-11-07 13:26:37 +0900

    7-7まで終了

--- Practice/src/practice07/PTra07_06.java
+++ Practice/src/practice07/PTra07_06.java
@@ -14,14 +14,21 @@ public class PTra07_06 {
 				{500,40,6},
 				{70,8,90,1000}
 		};
-
 		// ★ 配列arrayに格納されている全ての数字の平均を出力してください
-		for(int[] number : array ) {
-			int sum = 0;
-			for (int i : number) {
-				sum+=i;
+		//System.out.println((array[0][0]+array[0][1]+array[0][2]+array[1][0]+array[1][1]+array[1][2])/array.length);
+		int sumAll = 0;
+		
+		
+		for(int i = 0;i<array.length;i++) {
+				int sum = 0;
+			for (int j = 0;j<array[i].length;j++) {
+				
+				sum+= array[i][j];
+				
 			}
 			System.out.println(sum);
+			sumAll += sum;
 		}
+		System.out.println(sumAll/array.length);
 	}
 }

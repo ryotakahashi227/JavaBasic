823cc26a66e727ef55af23d63c35ef93bc30404a
LAPTOP-KUSH1VGI\ryo-t <ryo.takahashi.likeit@gmail.com>
2018-11-07 18:08:12 +0900

    8-6まで終了

--- Practice/src/practice08/PTra08_03.java
+++ Practice/src/practice08/PTra08_03.java
@@ -10,18 +10,28 @@ public class PTra08_03 {
 	public static void main(String[] args) {
 
 		// ★ 0～100のランダムな値を取得して、変数numberに代入してください
+		int number = new java.util.Random().nextInt(100);
+		
 
 
 		// ★ 変数resultを宣言し、メソッドisEvenNumberを（変数number）で呼び出した結果を代入してください
+		boolean result = isEvenNumber(number);
 
-
-		if (result) {
+		if (result==true) {
 			System.out.println("偶数です");
 		} else {
 			System.out.println("奇数です");
 		}
 	}
-
+		
+	
+		public static boolean isEvenNumber(int x ) {
+			if(x%2==0) {
+				return true;
+			}else {
+				return false;
+		}
+		}
 	/*
 	 * ★ 以下の仕様でメソッドを作成してください
 	 *

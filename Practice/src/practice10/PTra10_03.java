9a8bbcb7ac73adb75f428beb5d8f5a6ce57c6cfd
LAPTOP-KUSH1VGI\ryo-t <ryo.takahashi.likeit@gmail.com>
2018-11-08 18:20:21 +0900

    10-3まで終了

--- Practice/src/practice10/PTra10_03.java
+++ Practice/src/practice10/PTra10_03.java
@@ -16,27 +16,43 @@ public class PTra10_03 {
 
 		// Userクラスを作成後に着手してください
 		// ★ User型の変数usを宣言してください
-
-
+		User us ; 
 		// ★ 変数usに、Userクラスのインスタンスを作成・代入してください
-
-
+		us = new User();
+		
 		System.out.println("ユーザID（数字）、 ユーザ名、メールアドレス、パスワードをカンマ区切りで入力してください");
 
 		// ★ java.util.Scannerクラスをインスタンス化し、変数scannerに代入してください
-
+		
+		
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		 
 
 		// ★ 入力された値を、String型の変数lineに格納してください
-
+		
+		
+		String line = scanner.nextLine();
+		
+		
 
 		// ★ 変数lineに格納されている値を','で区切り、配列にしてください（Stringクラスのメソッドを使用します）
+		String[] lineTwo = line.split(",");
+			 
 
 
+		
 		// ★ 配列にした値を、usの各フィールドに代入してください
 		// ユーザIDに該当する値が数値でなかった場合は、エラーでプログラムが止まっても良いです
-
+		 us.userId = Integer.parseInt(lineTwo[0]);
+		 us.userNm = lineTwo[1];
+		 us.mail = lineTwo[2];
+		 us.password = lineTwo[3];
+		
 
 		// ★ 変数usに格納されているインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
-
+		 System.out.println(us.userId);
+		 System.out.println(us.userNm);
+		 System.out.println(us.mail);
+		 System.out.println(us.password);
 	}
 }
--- Practice/src/practice10/PTra10_04.java
+++ Practice/src/practice10/PTra10_04.java
@@ -15,30 +15,42 @@ public class PTra10_04 {
 	public static void main(String[] args) {
 
 		// ★ User型の変数が3つ入る配列usersを宣言・確保してください
-
+		
+		
+		User[] users = new User[3];
 
 		// ★ java.util.Scannerクラスをインスタンス化し、変数scannerに代入してください
-
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
 
 		// ★ usersのサイズ分繰り返す制御構文を記述してください
 		//----------- ループ start
-
-			System.out.println("##人目の情報を入力します");		/* ##には、カウントを入れてください */
+		int count= 0;
+		for (; count<3;) {
+			count++;
+			System.out.println(count+"人目の情報を入力します");		/* ##には、カウントを入れてください */
 			System.out.println("ユーザID（数字）、 ユーザ名、メールアドレス、パスワードをカンマ区切りで入力してください");
-
+		//この行に最初　}　が入っていた・・・
 			// ★ コマンドプロンプトに入力された値を、String型の変数lineに格納してください
-
+		String line = scanner.nextLine();
 
 			// ★ 変数lineに格納されている値を','で区切り、配列にしてください（Stringクラスのメソッドを使用します）
-
+		String[] lineTwo = line.split(",");
 
 			// ★ 配列にした値を、users[カウント]の各フィールドに代入してください
 			// ★ ユーザIDに該当する値が数値でなかった場合は、エラーでプログラムが止まっても良いです
-
-
+		
+		 for (String value : lineTwo) {
+	            System.out.println(value);
+		 }
+		
+		
+		
+		
+		
+	
 		//----------- ループ end
 
-		// ★ usersに格納されている全てのインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
-
+		}// ★ usersに格納されている全てのインスタンスの、フィールドuserId, userNm, mail, passwordを出力してください
+		 
 	}
 }
